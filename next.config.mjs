c78b19dbb4b7ce7382e436b7ce85230c2d1d2648
w_h_at <whatonscratch@gmail.com>
2025-04-21 18:54:05 +0100

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // <- This replaces `next export`
+  };
+  
+  export default nextConfig;
+  
