fd03f84c47e4926b959396c736a45d5b2282da3a
w_h_at <whatonscratch@gmail.com>
2025-04-21 20:57:56 +0100

    FUCKING WORK STUPID

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
      <p
   style={{
     position: 'fixed',
-    bottom: '48vh',
+    bottom: '50vh',
     left: '50%',
     transform: 'translateX(-50%)',
     fontSize: '3rem',
